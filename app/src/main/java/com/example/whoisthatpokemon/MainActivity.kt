05ec807315daf352f50c6c87238cc9f6a1c3a330
DagoPeralta94 <dagoperalta163@gmail.com>
2022-12-23 14:02:03 -0500

    Add Alert Dialog

--- app/src/main/java/com/example/whoisthatpokemon/MainActivity.kt
+++ app/src/main/java/com/example/whoisthatpokemon/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.graphics.drawable.toDrawable
 import androidx.core.widget.addTextChangedListener
 import com.example.whoisthatpokemon.data.DataPokemon
@@ -53,6 +54,32 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(txtPokeInput.context, "NOMBRE INCORRECTO", Toast.LENGTH_SHORT).show()
                 lives -= 1
                 binding.txtLive.text = lives.toString()
+                if(lives == 0) run {
+                    val builder = AlertDialog.Builder(txtPokeInput.context)
+                    builder.setTitle("GAME OVER")
+                    builder.setMessage("SCORE: ")
+                    builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+                    builder.setPositiveButton("Yes") { dialogInterface, which ->
+                        Toast.makeText(applicationContext, "clicked yes", Toast.LENGTH_LONG).show()
+                    }
+                    //performing cancel action
+                    builder.setNeutralButton("Cancel") { dialogInterface, which ->
+                        Toast.makeText(
+                            applicationContext,
+                            "clicked cancel\n operation cancel",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                    builder.setNegativeButton("No") { dialogInterface, which ->
+                        Toast.makeText(applicationContext, "clicked No", Toast.LENGTH_LONG).show()
+                    }
+                    val alertDialog: AlertDialog = builder.create()
+                    alertDialog.setCancelable(false)
+                    alertDialog.show()
+                    lives = 3
+                    binding.txtLive.text = lives.toString()
+                }
             }
         }
     }
